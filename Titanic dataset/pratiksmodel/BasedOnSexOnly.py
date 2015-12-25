2dc4925b4d49bfe30d2f5a52cfcc106682809fe2
Pratik S. Kabara <pshrikantkab@avaya.com>
2015-12-26 00:22:26 +0530

    Restructuring and Started creating new model

--- Titanic dataset/pratiksmodel/BasedOnSexOnly.py
+++ Titanic dataset/pratiksmodel/BasedOnSexOnly.py
@@ -8,17 +8,17 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn import metrics
 
 #read titanic train set
-train = pd.read_csv('train.csv')
+train = pd.read_csv('../train.csv')
 #print train.head()
 
 #read titanic test set
-test = pd.read_csv('test.csv')
+test = pd.read_csv('../test.csv')
 
 #read gender model answer set
-gtest = pd.read_csv('gendermodel.csv')
+gtest = pd.read_csv('../gendermodel.csv')
 
 #read gender class model answer set
-gctest =  pd.read_csv('genderclassmodel.csv')
+gctest =  pd.read_csv('../genderclassmodel.csv')
 
 #sexOfPeople = train.Sex
 #print type(sexOfPeople)
@@ -32,6 +32,6 @@ clf.fit(train[['Sex']].values, train['Survived'].values)
 test['Sex'] = test['Sex'].map(lambda x: 0 if x=='female' else 1 )
 predication = clf.predict(test[['Sex']].values)
 
-#print metrics.accuracy_score(predication, gtest['Survived'].values)
+print metrics.accuracy_score(predication, gtest['Survived'].values)
 
-#print metrics.accuracy_score(predication, gctest['Survived'].values)
+print metrics.accuracy_score(predication, gctest['Survived'].values)
