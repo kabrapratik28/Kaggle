03f92dc5d97420dae7c4980645e8a21f4fefa960
Pratik S. Kabara <pshrikantkab@avaya.com>
2015-12-26 01:30:25 +0530

    Model made on basis of sex, class and fare

--- Titanic dataset/pratiksmodel/BasedOnSexClassAndFare.py
+++ Titanic dataset/pratiksmodel/BasedOnSexClassAndFare.py
@@ -7,6 +7,7 @@ Created on Sat Dec 26 00:16:17 2015
 
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import Imputer
 from sklearn import metrics
 
 #read titanic train set
@@ -25,14 +26,27 @@ gctest =  pd.read_csv('../genderclassmodel.csv')
 #sexOfPeople = train.Sex
 #print type(sexOfPeople)
 
+# male =1 female =0
 train['Sex'] = train['Sex'].map(lambda x: 0 if x=='female' else 1 )
 
+listOfClmToBeConsidered = ['Sex','Pclass','Fare']
 
 clf = RandomForestClassifier(n_estimators=100, random_state=0)
-clf.fit(train[['Sex']].values, train['Survived'].values)
+clf.fit(train[listOfClmToBeConsidered].values, train['Survived'].values)
 
+#sex is not missing so converted.
 test['Sex'] = test['Sex'].map(lambda x: 0 if x=='female' else 1 )
-predication = clf.predict(test[['Sex']].values)
+
+#test contains nan value in fair
+#use Imputer
+#more info => row no. 152
+#replace nan => 35.6271884892
+imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
+imp.fit(test[listOfClmToBeConsidered].values)
+testingData = imp.transform(test[listOfClmToBeConsidered].values)
+
+
+predication = clf.predict(testingData)
 
 print metrics.accuracy_score(predication, gtest['Survived'].values)
 
