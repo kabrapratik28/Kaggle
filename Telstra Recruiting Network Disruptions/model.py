377ce3778bb2af3ca4a41d5beba3f658a8ca235f
Pratik S. Kabara <pshrikantkab@avaya.com>
2016-01-01 20:21:45 +0530

    Everything segregated and commented.

--- Telstra Recruiting Network Disruptions/model.py
+++ Telstra Recruiting Network Disruptions/model.py
@@ -9,51 +9,57 @@ log_feature = pd.read_csv('log_feature.csv')
 resource_type = pd.read_csv('resource_type.csv')
 severity_type = pd.read_csv('severity_type.csv')
 
-#pd.unique(event_type['event_type'].values)
-
+#===================== train and test related ===============================
 train = pd.read_csv('train.csv')
 test = pd.read_csv('test.csv')
+train['location'] =  train['location'].map(lambda x: int(x.strip('location ')))
+test['location'] =  test['location'].map(lambda x: int(x.strip('location ')))
 
-#inner join train and severity as 1 as 1 mapping.
+#===================== severity related ===============================
+#severity type only
+severity_type['severity_type_only'] =  severity_type['severity_type'].map(lambda x: int(x.strip('severity_type ')))
+#drop string severity column
+severity_type.drop(["severity_type"],axis=1,inplace =True)
+
+#inner join train,test and severity as 1 as 1 mapping.
 dataset_train = pd.merge(train, severity_type, on =['id'])
+dataset_test = pd.merge(test, severity_type, on =['id'])
 
-#derieved
-#take total number of events
+#===================== event type related ===============================
+#>>>>>>>>   derieved
+#>>>>>>>>   take total number of events
 event_type['event_type_count'] = 1
 total_no_of_events = event_type.groupby('id').event_type_count.sum()
 total_no_of_events = total_no_of_events.reset_index()
 #join on train
 dataset_train = pd.merge(dataset_train,total_no_of_events,on =['id'])
+dataset_test = pd.merge(dataset_test,total_no_of_events,on =['id'])
 
 #take numbers only.
 event_type['event_type_only'] = event_type['event_type'].map(lambda x: int(x.strip('event_type ')))
 #sort event type by id
-event_type_sorted = event_type.sort(['id'])
+#event_type_sorted = event_type.sort(['id'])
 
 #drop other frames
-event_type_sorted = event_type_sorted.drop(['event_type','event_type_count'],axis=1)
+event_type.drop(['event_type','event_type_count'],axis=1,inplace=True)
 
 # id and related list of events
 #http://stackoverflow.com/questions/22219004/grouping-rows-in-list-in-pandas-groupby
 #**** do not directlt assign **** bz id seq. not same.
-event_id_and_list = event_type_sorted.groupby('id')['event_type_only'].apply(lambda x: x.tolist())
+event_id_and_list = event_type.groupby('id')['event_type_only'].apply(lambda x: x.tolist())
 event_id_and_list = event_id_and_list.reset_index()
 
-dataset_train = pd.merge(dataset_train, event_id_and_list, on =['id'])
-
-'''
-def each_feature_maker(list_of_idex):
-    new_empty_list = [0] * 54   # 54 is number of features
-    for i in list_of_idex:
-        new_empty_list[i-1] = 1 
-    return new_empty_list
-    
-a = dataset_train["event_type_only"].apply(lambda x : each_feature_maker(x))
-'''
 #for each column check if it is in list for that row if yes then 1 else 0
 for i in range(1,55):
-    dataset_train["e"+str(i)] = dataset_train["event_type_only"].apply(lambda x : 1 if i in x else 0) 
+    event_id_and_list["e"+str(i)] = event_id_and_list["event_type_only"].apply(lambda x : 1 if i in x else 0) 
 
+#drop event type only list (Uncomment below to GOOD VISUALISE IN CSV)
+event_id_and_list.drop(['event_type_only'],axis=1,inplace=True)
+
+dataset_train = pd.merge(dataset_train, event_id_and_list, on =['id'])
+dataset_test = pd.merge(dataset_test, event_id_and_list, on =['id'])
+
+#===================== log feature related ===============================
 def assignVolumesColumns(x,log_feature):
     temp = 0
     for i in x :
@@ -61,7 +67,6 @@ def assignVolumesColumns(x,log_feature):
             temp =  i[1]
             break
     return temp
-
 
 #working on log features
 log_feature['log_feature_only'] = log_feature['log_feature'].map(lambda x: int(x.strip('feature ')))
@@ -75,7 +80,9 @@ for j in range(1,387):
 log_feature_grouped_final = log_feature_grouped.drop(["log_and_volume_all"],axis=1)
 #log_feature_grouped.to_csv("log_feature_grouped.csv")
 dataset_train = pd.merge(dataset_train,log_feature_grouped_final,on=['id'])
+dataset_test = pd.merge(dataset_test,log_feature_grouped_final,on=['id'])
 
+#===================== resource related ===============================
 #resource type 
 resource_type['resource_type_only'] = resource_type['resource_type'].map(lambda x: int(x.strip('resource_type ')))
 resource_type.drop(['resource_type'],axis=1,inplace=True)
@@ -88,26 +95,60 @@ for j in range(1,11):
 
 resource_type.to_csv("resource_related_all.csv")
 resource_type.drop(["resource_type_only"],axis=1,inplace=True)
+
 dataset_train = pd.merge(dataset_train,resource_type,on=['id'])
+dataset_test = pd.merge(dataset_test,resource_type,on=['id'])
 
-'''    
-#dataset_train.to_csv("testing.csv")
-dataset_train = dataset_train.drop(['event_type_only','location','severity_type'],axis=1)
+#=========================================================================
+#dataset_train.to_csv("dataset_train.csv")
+#dataset_test.to_csv("dataset_test.csv")
+#=========================================================================
 
-X_train, X_test = train_test_split(dataset_train, train_size=0.75)
+#===================== features considered for train and test related ===============================
+features_to_be_considered = []
+'''
+severity
+'''
+severity_f = ['severity_type_only'] 
+'''
+event type
+'''
+event_f = ['event_type_count'] 
+for e in range(1,55):
+    event_f.append('e'+str(e))
+'''
+log feature
+'''
+log_f = []
+for l in range(1,387):
+    log_f.append('f'+str(l))
+'''
+resource
+'''
+resource_f = ['resource_count'] 
+for r in range(1,11):
+    resource_f.append('r'+str(r))
 
-X_train_output = X_train['fault_severity']
-X_train_input = X_train.drop(['fault_severity'],axis=1)
+features_to_be_considered = severity_f + event_f + log_f + resource_f
 
-X_test_output = X_test['fault_severity']
-X_test_input = X_test.drop(['fault_severity'],axis=1)
+#===================== split data for train and test related ===============================
+X_train, X_test, Y_train,Y_test = train_test_split(dataset_train[features_to_be_considered],dataset_train['fault_severity'], train_size=0.75)
 
-rf = RandomForestClassifier(50)
-rf.fit(X_train_input.values,X_train_output.values)
+#===================== Algorithm to apply related ===============================
+model = RandomForestClassifier(50)
+model.fit(X_train.values,Y_train.values)
 
-output = rf.predict(X_test_input.values)
-print accuracy_score(X_test_output,output)
+output = model.predict(X_test.values)
+print accuracy_score(Y_test,output)
 
-
-#done with ... severity_type, event type and log feature  
+#====================== model to apply on actual test data =======================
+'''
+model.fit(dataset_train[features_to_be_considered].values,dataset_train['fault_severity'].values)
+outputFinal = model.predict(dataset_test[features_to_be_considered].values) 
+outputFinalFrame = pd.DataFrame({'id': dataset_test['id'],'output':outputFinal})
+for j in range(3):
+    outputFinalFrame['predict_'+str(j)] = outputFinalFrame["output"].apply(lambda x : 1 if j == x else 0) 
+outputFinalFrame.drop(['output'],axis=1,inplace=True)
+outputFinalFrame.set_index('id',inplace=True)
+outputFinalFrame.to_csv("output.csv")
 '''
